3e6045affb03bc5b4eb6a5fd7be2818c77bd26b5
Kajoua <yang.kajoua@gmail.com>
2022-08-14 13:20:28 +0200

    Products.js create

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,16 @@
 fetch("http://localhost:3000/api/products")
     .then((reponse) => reponse.json())
-    .then((secondReponse) => console.table(secondReponse));
+    .then((secondReponse) => {
+        secondReponse.forEach((element) => {
+            let html = `
+            <a href="./product.html?id=${element._id}">
+            <article>
+              <img src="${element.imageUrl}" alt="${element.altTxt}">
+              <h3 class="productName">${element.name}</h3>
+              <p class="productDescription">${element.description}</p>
+            </article>
+          </a>
+          `;
+            document.getElementById("items").innerHTML += html;
+        });
+    });
